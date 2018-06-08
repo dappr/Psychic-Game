86e9ac3b5c149760ba20c6696dc13ac0739963cf
Dappr <adon.hemley12@gmail.com>
2018-06-08 12:05:03 -0400

    update hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,67 @@
+
+// Create an array for the computer guesses
 var randomletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Variables for wins, losses, and guesses
 var guessletters = [];
 var guesssofar= ""
 var guessesleft = 9;
 var wins = 0
 var losses = 0 
 
-// random letters for compguess full alphabet 
-
-for (var i = 0; i < randomletters.length; i++); {
-    
-    if (event.key == compguess [i]) {
-       
-        guesssofar += userguess + ", ";
-
-        if (userguess == compguess) {
-            wins++;
-
-            guessesleft = 9;
-
-            compguess = randomletters [Math.floor(Math.random() * randomletters.length)];
-            guesssofar = "";
-        }
-
-    else if (userguess !== compguess) {
-        guessesleft--;
-        
-    }
-    }
-}
-
-
+//Functions
 document.onkeyup = function(event) {
 
     var userguess = event.key; 
     console.log("userguess" + userguess)
 };
 
-
-var compguess = randomletters [Math.floor(Math.random() * randomletters.length)];
+compguess = randomletters [Math.floor(Math.random() * randomletters.length)];
 
 console.log("compguess" + compguess)
+
+
+// For loop for computer choices 
+
+for (var i = 0; i < randomletters.length; i++); {
+ 
+    // Beginning of logic
+    // If condition to save user guesses
+    if (event.key == randomletters[i] ){
+       
+        guesssofar += userguess + ",";
+
+        // If condition when user guesses correctly
+        if (userguess == compguess) {
+            wins++;
+
+         // If condition for guesses left
+            guessesleft = 9;
+
+            compguess = randomletters [Math.floor(Math.random() * randomletters.length)];
+            guesssofar = "";
+        }
+
+        // Else if condition for when user guesses incorrectly
+    else if (userguess !== compguess) {
+        guessesleft--;
+    
+    
+            // When user runs out of guesse they will get a loss.
+            if (guessesleft == 0 ) {
+
+                losses++;
+                guessesleft = 9;
+                compguess = randomletters [Math.floor(Math.random() * randomletters.length)];
+                guesssofar = "";
+
+    
+                document.querySelector("psychic-game").innerHTML = html;
+            }    }
+    }
+}
+
+
 
 
 
